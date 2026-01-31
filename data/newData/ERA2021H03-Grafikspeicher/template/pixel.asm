df82ebb5a744098b35615cc72afcf748e70fec72
khinevich <ge65nub@mytum.de>
2026-01-31 13:54:06 +0100

    fix ERA2021H03-Grafikspeicher: correct spelling and formatting in pixel.asm

--- data/newData/ERA2021H03-Grafikspeicher/template/pixel.asm
+++ data/newData/ERA2021H03-Grafikspeicher/template/pixel.asm
@@ -9,7 +9,7 @@
 ;Input values with more than 4 digits will partially stay in stdin and be read later which leads to wrong output
 
 ;Usage in SASM: Input from stdin has to be put in the input field in SASM before execution
-;Additionally in SASM all input values have to have the right size which means 4 digits for x and y, 8 digits for the RGB color (preceding zeros, e.g. 0001) 
+;Additionally in SASM all input values have to have the right size which means 4 digits for x and y, 8 digits for the RGB color (preceding zeros, e.g. 0001)
 
 
 ;---TODO: INSERT FORMULA HERE: X
@@ -20,12 +20,12 @@
 section .bss
     display_start RESB 8294400
     buf resb 16
-section .data 
-    message db 10, "Wert in Speicherzelle (0x%08X, 0x%08X) = 0x%08X", 10, 10, 0
+section .data
+    message db "Wert in Speicherzelle (0x%08X, 0x%08X) = 0x%08X", 10, 10, 0
     value db "Wert: 0x%08X", 10, 0
     get_x db "Bitte Wert für x eingeben", 10, 0
     get_y db "Bitte Wert für y eingeben", 10, 0
-    get_color db "Bitte RBG-Wert für neue Farbe eingeben", 10, 0
+    get_color db "Bitte RGB-Wert für neue Farbe eingeben", 10, 0
 section .text
 global main
 extern printf
@@ -34,10 +34,10 @@ extern fgets
 ;---------------------------------------
 
 ;---TODO implement set_pixel here---
-set_pixel: 
+set_pixel:
         ret
-        
-        
+
+
 ;------------------------------
 
 ;-------Do not modify anything below except the one TODO-------
@@ -48,34 +48,34 @@ main:
  push ebx
  push edi
  push esi
- 
+
  ;print instruction
  push get_x
  call printf
  add esp, 4
- 
+
  ;syscall to read x from stdin
  mov eax, 3      ; sys_read
  mov ebx, 0      ; stdin
- mov ecx, buf    ; buffer 
+ mov ecx, buf    ; buffer
  mov edx, 5      ; size in byte
  int 80h
- 
- ;convert x from ASCII to int 
+
+ ;convert x from ASCII to int
  push buf
  call atoi
  add esp, 4
  push eax
- 
+
  ;print instruction
  push get_y
  call printf
  add esp, 4
- 
+
  ;clear buffer
  mov dword [buf], 0
  mov dword [buf + 4], 0
- 
+
  ;syscall to read y from stdin
  mov eax, 3      ; sys_read
  mov ebx, 0      ; stdin
@@ -91,7 +91,7 @@ main:
  ;mov values to right registers
  mov ebx, eax
  pop eax
- 
+
  ;save values for later
  push eax
  push ebx
@@ -102,50 +102,50 @@ main:
 
  ;get coordinates
  pop ebx
- pop eax 
- 
+ pop eax
+
  ;save coordinates and position for later
  push eax
  push ebx
  push esi
- 
+
  ; print initial value at position
  push ecx
  push ebx
  push eax
  push message
  call printf
- add esp, 16 
+ add esp, 16
 
- 
+
  ; print instruction
  push get_color
  call printf
  add esp, 4
- 
+
  ; clear buffer
  mov dword [buf], 0
  mov dword [buf + 4], 0
- 
+
  ;read color from stdin
  mov eax, 3      ; sys_read
  mov ebx, 0      ; stdin
  mov ecx, buf    ; buffer (memory address, where read should save 4 byte)
  mov edx, 9      ; read byte count
  int 80h
- 
- ;convert color from ASCII to int   
+
+ ;convert color from ASCII to int
  push buf
  call atoi
  add esp, 4
  mov ecx, eax
-  
+
  ;get coordinates
  mov eax, [esp +8]
- mov ebx, [esp + 4] 
- 
+ mov ebx, [esp + 4]
+
  call set_pixel
- 
+
  ;get position and coordinates
  pop edx
  pop ebx
@@ -161,7 +161,7 @@ main:
  push message
  call printf
  add esp, 16
- 
+
  ;restore values
  pop esi
  pop edi
